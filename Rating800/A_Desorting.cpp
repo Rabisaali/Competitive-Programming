b7da95ce47002167862053edb6028bf9354bd1aa
Rabisa <rabisa55511@gmail.com>
2026-01-04 19:47:47 +0500

    adding cp-31 800 rated questions

--- Rating800/A_Desorting.cpp
+++ Rating800/A_Desorting.cpp
@@ -11,21 +11,12 @@ int main () {
         for (int i=0; i<n; i++) {
             cin >> a[i];
         }
-        int diff;
-        if (a[n-1]>a[n-2]) diff = a[n-1]-a[n-2];
-        else if (a[n-1]==a[n-2]) {
-            cout << "1\n";
-            continue;
+        int i=0; 
+        while (a[n-2]<=a[n-1]) {
+            a[n-2]++;
+            a[n-1]--;
+            i++;
         }
-        else diff = a[n-2]-a[n-1];
-        if (diff==1) {
-            cout << "0\n";
-            continue;
-        }
-        else {
-            diff = diff - (diff/2);
-            cout << diff <<"\n";
-        }
-
+        cout << i << "\n";
     }
 }
