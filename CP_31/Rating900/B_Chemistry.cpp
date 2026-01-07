195664b406e5bfdc1060a0d0ec40017be577aa83
Rabisa <rabisa55511@gmail.com>
2026-01-07 23:59:46 +0500

    adding hello2026 contest problem1

---
+++ CP_31/Rating900/B_Chemistry.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+int main () {
+    LL t;
+    cin >> t;
+    while (t--) {
+        LL n, k;
+        cin >> n >> k;
+        char arr[n];
+        for (int i=0; i<n; i++) {
+            cin >> arr[i];
+        }
+        int diff = n-k;
+        if (diff%2 != 0) {
+            
+        } 
+    }
+}
