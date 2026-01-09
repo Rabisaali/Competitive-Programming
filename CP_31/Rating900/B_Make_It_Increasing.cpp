6f73a86d66cb0c1704dafb9b85ed8a844718dfe4
Rabisa <rabisa55511@gmail.com>
2026-01-09 21:05:01 +0500

    adding cp-31 900 rated questions

---
+++ CP_31/Rating900/B_Make_It_Increasing.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+int main () {
+    LL t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector <int> a(n);
+        for (int i=0; i<n; i++) {
+            cin >> a[i];
+        }
+        int ans=0;
+        int flag=0;
+        for (int i=n-2; i>=0; i--) {
+            while (a[i]>=a[i+1] && a[i]>0) {
+                a[i]/=2;
+                ans++;
+            }
+            if (a[i]==a[i+1]) {
+                flag=1;
+                cout << -1 << "\n";
+                break;
+            } 
+        }
+        if (flag==0) cout << ans << "\n";
+    }
+}
