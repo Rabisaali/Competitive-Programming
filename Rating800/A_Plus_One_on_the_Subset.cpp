29abf629bc020e04120922d2c37ff2158d9a8cc0
Rabisa <rabisa55511@gmail.com>
2026-01-10 17:23:31 +0500

    adding 800 rated questions

---
+++ Rating800/A_Plus_One_on_the_Subset.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+int main () {
+    LL t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector <int> a(n);
+        for (int i=0; i<n; i++) {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        int ans = a[n-1]-a[0];
+        cout << ans << "\n";
+    }
+}
