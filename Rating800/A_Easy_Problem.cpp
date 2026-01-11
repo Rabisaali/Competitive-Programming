bcf4b5088c5812df326cd327cd3afccaeb47185b
Rabisa <rabisa55511@gmail.com>
2026-01-11 20:19:58 +0500

    adding 800 rated questions

---
+++ Rating800/A_Easy_Problem.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << n-1 << "\n";
+    }
+}
