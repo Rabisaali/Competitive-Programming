8c3e38ef4dcd3d61f5ecd0bb3322de40dce1bee6
Rabisa <rabisa55511@gmail.com>
2026-01-14 22:55:16 +0500

    adding 900 rated ques of cp-31 sheet

--- CP_31/Rating900/B_Chemistry.cpp
+++ CP_31/Rating900/B_Chemistry.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 #define LL long long int
 int main () {
-    LL t;
+    int t;
     cin >> t;
     while (t--) {
         LL n, k;
         cin >> n >> k;
-        char arr[n];
+        char s[n];
+        map<char, int>a;
         for (int i=0; i<n; i++) {
-            cin >> arr[i];
+            cin >> s[i];
+            a[s[i]]++;
         }
-        int diff = n-k;
-        if (diff%2 != 0) {
-            
-        } 
+        int count=0;
+        for (auto& pair: a) {
+            if (pair.second % 2 != 0) count++;
+        }
+        
+            if (count-1 <= k) cout << "YES\n";
+            else cout << "NO\n";
     }
 }
