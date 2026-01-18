640ac356f0a61350588ffbfe46acb13756281b86
Rabisa <rabisa55511@gmail.com>
2026-01-18 23:28:25 +0500

    adding div 2 round 1073

---
+++ Contests/Round1073(Div2)/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        sort(a.begin(), a.end());
+        a.erase(unique(a.begin(), a.end()), a.end());
+
+        int best = 1, cur = 1;
+        for (int i = 1; i < (int)a.size(); i++) {
+            if (a[i] == a[i-1] + 1) {
+                cur++;
+            } else {
+                cur = 1;
+            }
+            best = max(best, cur);
+        }
+
+        cout << best << "\n";
+    }
+}
