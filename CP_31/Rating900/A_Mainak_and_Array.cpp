20a21409e08fb68c0a436363dfedab738d9a1a69
Rabisa <rabisa55511@gmail.com>
2026-01-18 23:23:20 +0500

    adding 900 rated ques of cp-31 sheet

--- CP_31/Rating900/A_Mainak_and_Array.cpp
+++ CP_31/Rating900/A_Mainak_and_Array.cpp
@@ -11,10 +11,6 @@ int main () {
         for (int i=0; i<n; i++) {
             cin >> a[i];
         }
-        if (n==1) {
-            cout << "0\n";
-            continue;
-        }
         sort(a.begin(), a.end());
         int ans = a[n-1]-a[0];
         cout << ans << "\n";
