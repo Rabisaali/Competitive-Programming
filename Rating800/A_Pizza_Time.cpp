401b08fba7ad54de0e16c8f1b0f0ed3ceae0c373
Rabisa <rabisa55511@gmail.com>
2026-01-29 21:42:09 +0500

    adding 800 rated ques

---
+++ Rating800/A_Pizza_Time.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+int main () {
+    int t;
+    cin >> t;
+    while (t--) {
+        LL n;
+        cin >> n;
+        cout << (n-1)/2 << "\n";
+    }
+}
