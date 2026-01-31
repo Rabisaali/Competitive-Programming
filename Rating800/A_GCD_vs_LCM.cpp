81cb792c8b5fa6fd66851c937c0986e0c40fbf95
Rabisa <rabisa55511@gmail.com>
2026-01-31 23:29:37 +0500

    adding 800 rated ques

---
+++ Rating800/A_GCD_vs_LCM.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long int
+int main () {
+    LL t;
+    cin >> t;
+    while (t--) {
+        LL n;
+        cin >> n;
+        cout << n-3 << " 1 1 1\n";
+    }
+}
