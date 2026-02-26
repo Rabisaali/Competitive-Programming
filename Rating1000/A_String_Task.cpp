0b8d31bbe4bc5676dc658b7c88575abccb727435
Rabisa <rabisa55511@gmail.com>
2026-02-26 23:21:53 +0500

    adding 1000 rated ques

---
+++ Rating1000/A_String_Task.cpp
@@ -0,0 +1,60 @@
+//Bismillah
+#include <bits/stdc++.h>
+#define nl endl;
+using namespace std;
+
+#define YN(condition) cout<<(condition?"YES":"NO")
+#define yn(condition) cout<<(condition?"Yes":"No")
+
+#define int long long
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpi vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mp map<int,int>
+#define uset unordered_set<int>
+#define ump unordered_map<int,int>
+
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define fn(i,s,e) for(int i=s;i>=e;i++)
+#define printv(vec) for(auto &value: vec) cout<<value<<endl;
+#define inputv(vec) for(auto &value: vec) cin>>value;
+
+#define pb push_back
+#define pp pop_back
+#define eb emplace_back
+#define all(s) s.begin(), s.end()
+#define sa(vec) sort(vec.begin(), vec.end())
+
+bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
+ll modexp(ll a, ll b, ll m) { ll res = 1; a %= m; while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; } return res; }
+#define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);int t = 1;
+// void solve () {
+    
+// }
+signed main () {
+    fastnuces;
+    // cin >> t;
+    // while (t--) {
+    //     solve();
+    // }
+    string s;
+    cin >> s;
+    string st;
+    int j=0;
+    int len = s.length();
+    f(i, 0, len) {
+        if (toupper(s[i])=='A' || toupper(s[i])=='E' || (toupper(s[i]))=='I' || (toupper(s[i]))=='O' || (toupper(s[i]))=='U' || (toupper(s[i]))=='Y') continue;
+        else {
+            st.push_back('.');
+            st.push_back(tolower(s[i]));
+        }
+    }
+    cout << st << "\n";
+    return 0;
+}
