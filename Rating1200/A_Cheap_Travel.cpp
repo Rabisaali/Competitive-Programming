c8491cf325d905be36ac40925c0495d350e6eecc
Rabisa <rabisa55511@gmail.com>
2026-03-10 21:30:03 +0500

    adding 1200 rated prob+updating the solution to prb d of last round

--- Rating1200/A_Cheap_Travel.cpp
+++ Rating1200/A_Cheap_Travel.cpp
@@ -14,7 +14,7 @@ using namespace std;
 #define vi vector<int>
 #define vc vector<char>
 #define vs vector<string>
-#define vpi vector<pair<int,char>>
+#define vpi vector<pair<int,int>>
 #define vvi vector<vector<int>>
 #define mp map<int,int>
 #define uset unordered_set<int>
@@ -33,42 +33,21 @@ using namespace std;
 
 bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
 ll modexp(ll a, ll b, ll m) { ll res = 1; a %= m; while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; } return res; }
-#define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-void solve () {
-    int n;
-    cin >> n;
-    vector<pair<int, char>>a(n);
-    f(i, 0, n) {
-        int first;
-        char second;
-        cin >> first >> second;
-        a[i] = {first, second};
-    }
-    //for (int i=0; i<n; i++) cout << a[i].first << " " << a[i].second << "\n";
-    sa(a);
-    int count = 0;
-    f(i, 0, n) {
-        if (a[i].second == '0') continue;
-        // int val = a[i].first;
-        for(int j=i+1; j<n && a[j].first == a[i].first; j++) {
-            if (a[j].second == '0') continue;
-            if (a[j].second != a[i].second) {
-                count++;
-                a[i].second = '0';
-                a[j].second = '0';
-                break;
-            }
-        } 
-    }
-    cout << count << "\n";
-
-}
+#define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);int t = 1;
+// void solve () {
+    
+// }
 signed main () {
     fastnuces;
-    // int t;
     // cin >> t;
     // while (t--) {
-        solve();
-    //}
+    //     solve();
+    // }
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+    int ans = b*(n/m)+(n%m)*a;
+    int ans1 = b*(n/m)+b;
+    int ans2 = a*n;
+    cout << min({ans, ans1, ans2}) << "\n";
     return 0;
 }
