a3abe11f20177f8b8227a145a6c690a0fcd72748
Rabisa <rabisa55511@gmail.com>
2026-04-24 14:17:37 +0500

    adding 1100 rated prob

---
+++ CP_31/Rating1100/B_Maximum_Sum.cpp
@@ -0,0 +1,70 @@
+//Bismillah
+#include <bits/stdc++.h>
+#define nl endl;
+using namespace std;
+
+#define YN(condition) cout<<(condition?"YES":"NO")
+#define yn(condition) cout<<(condition?"Yes":"No")
+
+#define int long long
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpi vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mp map<int,int>
+#define uset unordered_set<int>
+#define ump unordered_map<int,int>
+
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define fn(i,s,e) for(int i=s;i>=e;i--)
+#define printv(vec) for(auto &value: vec) cout<<value<<endl;
+#define inputv(vec) for(auto &value: vec) cin>>value;
+
+#define pb push_back
+#define pp pop_back
+#define eb emplace_back
+#define all(s) s.begin(), s.end()
+#define sa(vec) sort(vec.begin(), vec.end())
+
+bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
+ll modexp(ll a, ll b, ll m) { ll res = 1; a %= m; while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; } return res; }
+#define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);int t = 1;
+void solve () {
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    inputv (a);
+    sa(a);
+    vi prefix(n+1);
+    prefix[0] = a[0];
+    f(i, 1, n) {
+        prefix[i] += (prefix[i-1]+a[i]);
+    }
+    int count=0;
+    // int counter1=n-1;
+    // int counter2=n-1;
+    f(i, 0, k+1) {
+        int index = k-i;
+        int count1 = 2*i;
+        int count2 = n-index-1;
+        int sum = prefix[count2] - (count1==0 ? 0 : prefix[count1-1]);
+        count = max(count, sum);
+    }
+    cout << count << "\n";
+    
+}
+signed main () {
+    fastnuces;
+    //freopen(".in", "r", stdin);
+    //freopen(".out", "w", stdout);
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
