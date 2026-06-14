1261195b236f59503960cb5160c1977c86268b8f
Rabisa <rabisa55511@gmail.com>
2026-06-14 19:01:26 +0500

    adding todays div 3 round 1103

---
+++ Contests/Codeforces/Round1103(Div3)/D_Brand_New_Tatar_TV_Show.cpp
@@ -0,0 +1,77 @@
+//Bismillah
+#include <bits/stdc++.h>
+#define nl endl;
+using namespace std;
+
+#define YN(condition) cout<<(condition?"YES":"NO")
+#define yn(condition) cout<<(condition?"Yes":"No")
+
+#define int long long
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpi vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mp map<int,int>
+#define uset unordered_set<int>
+#define ump unordered_map<int,int>
+
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define fn(i,s,e) for(int i=s;i>=e;i--)
+#define printv(vec) for(auto &value: vec) cout<<value<<endl;
+#define inputv(vec) for(auto &value: vec) cin>>value;
+
+#define pb push_back
+#define pp pop_back
+#define eb emplace_back
+#define all(s) s.begin(), s.end()
+#define sa(vec) sort(vec.begin(), vec.end())
+
+bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
+ll modexp(ll a, ll b, ll m) { ll res = 1; a %= m; while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; } return res; }
+#define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);int t = 1;
+void solve () {
+    int n, k;
+    cin >> n >> k;
+    
+    vi a(n);
+    inputv(a);
+    sa(a);
+    vpi A = {{a[0], 1}};
+    f(i, 1, n) {
+        if (a[i] == a[i-1]) A.back().second++;
+        else A.eb(a[i], 1);
+    }
+    while (A.size()>0) {
+        n=A.size();
+        if (A[n-1].second%2==0) {
+            cout << "YES\n";
+            return;
+        }
+        if (n==1) {
+            cout << "NO\n";
+            return;
+        }
+        if (A[n-1].first-A[n-2].first<=k) {
+            cout << "YES\n";
+            return;
+        }
+        A.pop_back();
+    }
+
+
+}
+signed main () {
+    fastnuces;
+    //freopen(".in", "r", stdin);
+    //freopen(".out", "w", stdout);
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
