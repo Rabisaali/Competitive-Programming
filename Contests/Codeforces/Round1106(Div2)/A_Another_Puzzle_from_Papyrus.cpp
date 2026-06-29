b39a053aa84b97274d46638d823c34a0bf761750
Rabisa <rabisa55511@gmail.com>
2026-06-29 11:20:30 +0500

    uploading round 1106 after upsolving

---
+++ Contests/Codeforces/Round1106(Div2)/A_Another_Puzzle_from_Papyrus.cpp
@@ -0,0 +1,89 @@
+//Bismillah
+#include <bits/stdc++.h>
+#define nl endl;
+using namespace std;
+
+#define YN(condition) cout<<(condition?"YES":"NO")
+#define yn(condition) cout<<(condition?"Yes":"No")
+
+#define int long long
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vpi vector<pair<int,int>>
+#define vvi vector<vector<int>>
+#define mp map<int,int>
+#define uset unordered_set<int>
+#define ump unordered_map<int,int>
+
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define fn(i,s,e) for(int i=s;i>=e;i--)
+#define printv(vec) for(auto &value: vec) cout<<value<<endl;
+#define inputv(vec) for(auto &value: vec) cin>>value;
+
+#define pb push_back
+#define pp pop_back
+#define eb emplace_back
+#define all(s) s.begin(), s.end()
+#define sa(vec) sort(vec.begin(), vec.end())
+#define sr(vec) sort(vec.begin(), vec.end(), greater<int>())
+
+bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
+ll modexp(ll a, ll b, ll m) { ll res = 1; a %= m; while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; } return res; }
+#define fastnuces ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);int t = 1;
+void solve () {
+    int n, c;
+    cin >> n >> c;
+    int suma=0, sumb=0;
+    
+    vi a(n);
+
+    f(i, 0, n) {
+        cin >> a[i];
+        suma+=a[i];
+    }
+    vi b(n);
+    f(i, 0, n) {
+        cin >> b[i];
+        sumb+=b[i];
+    }
+    int ans = suma-sumb;
+    //int ans2=0;
+    if (ans<0) cout << "-1\n";
+    else {
+        bool flag1=true, flag2=true;
+        f(i, 0, n) {
+            if(a[i]<b[i]) {
+                flag1=false;
+                break;
+            }
+        }
+        sa(a);
+        sa(b);
+        
+        f(i, 0, n) {
+            if(a[i]<b[i]) {
+                flag2=false;
+                break;
+            }
+            //ans2 += a[i]-b[i];
+        }
+        if (flag1 && flag2) cout << ans << "\n";
+        else if (!flag1 && flag2) cout << ans+c << "\n";
+        else cout << "-1\n";
+    }
+}
+signed main () {
+    fastnuces;
+    //freopen(".in", "r", stdin);
+    //freopen(".out", "w", stdout);
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
